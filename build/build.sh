42118eb63b839c7563862813186925627eca04d0
Linus Torvalds <linustorvalds@linux.com>
2025-05-28 11:22:27 -0400

    dd

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,3 @@
-apk add git make cmake libstdc++ gcc g++ automake libtool autoconf linux-headers
-git clone https://github.com/xmrig/xmrig.git
 mkdir xmrig/build && cd xmrig/scripts
 ./build_deps.sh && cd ../build
 cmake .. -DXMRIG_DEPS=scripts/deps -DBUILD_STATIC=ON -DWITH_EMBEDDED_CONFIG=ON
